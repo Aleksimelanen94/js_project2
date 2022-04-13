977477e96b4d4755fb242854279f1801fabc944b
aleksimelanen94 <1802185@laurea.fi>
2022-04-13 19:28:26 +0300

    day two filtering tests

--- ajax.js
+++ ajax.js
@@ -11,19 +11,25 @@ xmlhttp.onreadystatechange = function(){
 
         var titles = xmlDoc.getElementsByTagName("Title");
         var theaters = xmlDoc.getElementsByTagName("Theatre");
+        var date = xmlDoc.getElementsByTagName("dttmShowStart");
 
-        var selection = document.getElementById("theaterSelection");
-        for(i=0; i < theaters.length; i++){
-            var option = document.createElement('option');
-            option.setAttribute('value', theaters[i].value);
-            option.appendChild(document.createTextNode(theaters[i].textContent));
-            selection.appendChild(option);
-        }
+        //var selection = document.getElementById("theaterSelection");
+        //for(i=0; i < theaters.length; i++){
+            //var option = document.createElement('option');
+            //option.setAttribute('value', theaters[i].value);
+            //option.appendChild(document.createTextNode(theaters[i].textContent));
+            //selection.appendChild(option);
+        //}
         
-        var txt = "<table border='1'>";
+        var txt ="";
+        txt = "<table border='1'>";
         for(i=0; i < titles.length; i++){
-            txt += "<tr><td>" + titles[i].childNodes[0].nodeValue + "</td>" + 
-            "<td>" + theaters[i].childNodes[0].nodeValue + "</td></tr>";
+
+            if(theaters[i].childNodes[0].nodeValue = "Sello, Espoo")
+            {
+                txt += "<tr><td>" + titles[i].childNodes[0].nodeValue + "</td>" + 
+                "<td>" + date[i].childNodes[0].nodeValue + "</td></tr>";
+            }
         }
 
         txt += "</table>";
@@ -35,4 +41,31 @@ xmlhttp.onreadystatechange = function(){
 
 function loadXMLfile(){
 
+            //clear div from previous information
+    document.getElementById("myDiv").innerHTML = "";
+
+    if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+
+        //document.getElementById("myDiv").innerHTML = xmlhttp.responseText;
+        var xmlDoc = xmlhttp.responseXML;
+
+        var titles = xmlDoc.getElementsByTagName("Title");
+        var theaters = xmlDoc.getElementsByTagName("Theatre");
+        var date = xmlDoc.getElementsByTagName("dttmShowStart");
+
+        var txt = "";
+        txt = "<table border='1'>";
+        for(i=0; i < titles.length; i++){
+            
+            if(theaters[i].childNodes[0].nodeValue = "Flamingo, Vantaa")
+            {
+                txt += "<tr><td>" + titles[i].childNodes[0].nodeValue + "</td>" + 
+                "<td>" + date[i].childNodes[0].nodeValue + "</td></tr>";
+            }
+        }
+
+        txt += "</table>";
+
+        document.getElementById("myDiv").innerHTML = txt;
+    }
 }
